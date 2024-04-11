9f9677668fd3787ab344e48dd6c8d1e13e1612c1
hernando <zzinc4869@outlook.com>
2024-04-11 15:27:52 +0800

    add level reserse alg

--- alg/tree_node_test.go
+++ alg/tree_node_test.go
@@ -64,6 +64,44 @@ func levelOrder(root *TreeNode) {
 	}
 }
 
+// 奇数层从左到右遍历，偶数层从右到左遍历
+func levelOrderReverse(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	queue := []*TreeNode{root}
+	level := 1
+	result := make([][]int, 0)
+
+	for len(queue) > 0 {
+		size := len(queue)
+		levelValues := make([]int, size)
+
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if level%2 == 1 {
+				levelValues[i] = node.val
+			} else {
+				levelValues[size-1-i] = node.val
+			}
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, levelValues)
+
+		level++
+	}
+	fmt.Println(result)
+}
+
 // 计算二叉树两节点的最长距离
 func maxDistance(root *TreeNode) int {
 	if root == nil {
@@ -105,6 +143,16 @@ func TestTreeNode(t *testing.T) {
 	root.right = &TreeNode{val: 3}
 	root.left.left = &TreeNode{val: 4}
 	root.left.right = &TreeNode{val: 5}
+	root.right.left = &TreeNode{val: 6}
+	root.right.right = &TreeNode{val: 7}
+	root.left.left.left = &TreeNode{val: 8}
+	root.left.left.right = &TreeNode{val: 9}
+	root.left.right.left = &TreeNode{val: 10}
+	root.left.right.right = &TreeNode{val: 11}
+	root.right.left.left = &TreeNode{val: 12}
+	root.right.left.right = &TreeNode{val: 13}
+	root.right.right.left = &TreeNode{val: 14}
+	root.right.right.right = &TreeNode{val: 15}
 
 	fmt.Println("前序遍历结果：")
 	preorderTraversal(root)
@@ -117,6 +165,9 @@ func TestTreeNode(t *testing.T) {
 
 	fmt.Println("层序遍历结果：")
 	levelOrder(root)
+
+	fmt.Println("层序遍历每层翻转结果：")
+	levelOrderReverse(root)
 
 	maxDistance(root)
 }
